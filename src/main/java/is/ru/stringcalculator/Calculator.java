85c7500fc87709a72a30ce88f0d2e09f10a68fb8
bennisverrir <bennisverrir@gmail.com>
2017-10-15 19:26:46 +0000

    refactored the add method and added the extract delimeter method

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,6 +2,7 @@ package is.ru.stringcalculator;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 
 public class Calculator
@@ -14,17 +15,9 @@ public class Calculator
 		}
 		else
 		{
-			if(text.startsWith("//"))
-			{
-				String[] temp = text.split("\n", 2);
-				String delimeter = temp[0];
-				delimeter = delimeter.replace("//","");
-				String rest = temp[1];
-				String numbers[] = rest.split(delimeter);
-				check(numbers);
-				return sum(numbers);
-			}
-			String numbers[] = text.split(",|\\n");
+			String[] extracted = extractDelimeter(text);
+
+			String numbers[] = extracted[1].split(extracted[0]);
 			check(numbers);
 			return sum(numbers);
 		}
@@ -70,6 +63,26 @@ public class Calculator
 				}
 			}
 			throw new IllegalArgumentException(message);
+		}
+	}
+
+	private static String[] extractDelimeter(String text)
+	{
+		String defaultDelimeter = ",|\\n";
+		if(text.startsWith("//"))
+		{
+			String[] extracted = text.split("\n", 2);
+			extracted[0] = extracted[0].replace("//","");
+			extracted[0] = StringUtils.replaceOnce(extracted[0], "\n","");
+			extracted[0] = extracted[0] + "|" + defaultDelimeter;
+			return extracted;
+		}
+		else
+		{
+			String[] temp = new String[2];
+			temp[0] = defaultDelimeter;
+			temp[1] = text;
+			return temp;
 		}
 	}
 }
